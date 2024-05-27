4ac57da10e2ac892c79fd3c2b91bc92dad695e48
Miguel-Paredes <os.miguelparedes@gmail.com>
2024-05-27 17:37:37 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+// import { SplashScreen } from '@capacitor/splash-screen';
+// import { StatusBar } from '@capacitor/status-bar';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+
+import { environment } from '../environments/environment';
+
+@NgModule({
+  declarations: [AppComponent],
+  // entryComponents: [],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+  ],
+  providers: [
+    // StatusBar,
+    // SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,38 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+
+export const environment = {
+  production: false,
+  // firebaseConfig : {
+  //   apiKey: "AIzaSyAhPdUzPj_MACEK915sxMdBPkxPS_wT5l8",
+  //   authDomain: "trabajo-75e99.firebaseapp.com",
+  //   databaseURL: "https://trabajo-75e99-default-rtdb.firebaseio.com",
+  //   projectId: "trabajo-75e99",
+  //   storageBucket: "trabajo-75e99.appspot.com",
+  //   messagingSenderId: "681609830261",
+  //   appId: "1:681609830261:web:3df7ea0b4014a047b97d3c",
+  //   measurementId: "G-B8YJ85SFPT"
+  // }
+  firebaseConfig : {
+    apiKey: "AIzaSyCGtF4FJjZKE1_7145pt4nFsW98hLi_QrI",
+  authDomain: "a-aa1b2.firebaseapp.com",
+  databaseURL: "https://a-aa1b2-default-rtdb.firebaseio.com",
+  projectId: "a-aa1b2",
+  storageBucket: "a-aa1b2.appspot.com",
+  messagingSenderId: "340088945622",
+  appId: "1:340088945622:web:1b71dfe349b05b239c0277",
+  measurementId: "G-T048FCYN8K"
+  }
+   
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
